e46f835ec67f65990e50e04528c8fd32b262dd1d
Manoj <laptop@local.com>
2026-04-02 21:12:51 +0530

    updated version.tf file

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.0"
 
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstatestorageaccount"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
+#  backend "azurerm" {
+#    resource_group_name  = "rg-terraform-state"
+#    storage_account_name = "tfstatestorageaccount"
+#    container_name       = "tfstate"
+#    key                  = "terraform.tfstate"
+#  }
 }
