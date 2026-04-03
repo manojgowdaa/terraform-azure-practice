f44ccdeeb22e08e3be787ab32b859c4b94ac6794
Manojkumar <vmanojk@apac.corpdir.net>
2026-04-03 16:09:03 +0530

    updated app service plan

--- modules/create_app_service/main.tf
+++ modules/create_app_service/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "app_plan" {
   resource_group_name = var.resource_group_name
   location            = var.location
   os_type             = "Linux"
-  sku_name            = "F1"
+  sku_name            = "B1"
 
   tags = {
     Name = var.app_service_plan_name
