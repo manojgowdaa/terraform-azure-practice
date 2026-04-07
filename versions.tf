974ebe90d8595b1694d1e6a6ac070e24d1bf8087
Manojkumar <vmanojk@apac.corpdir.net>
2026-04-07 11:11:46 +0530

    added backend for state

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.0"
 
-#  backend "azurerm" {
-#    resource_group_name  = "rg-terraform-test-2026"
-#    storage_account_name = "storageacct9876543"
-#    container_name       = "tfstate"
-#    key                  = "terraform.tfstate"
-#  }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-test-2026"
+    storage_account_name = "storageacct9876543"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
