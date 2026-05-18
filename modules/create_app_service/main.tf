d8492bcf6db70e65fd11d9c8cc0d5cd93a5a4940
Manojkumar <vmanojk@apac.corpdir.net>
2026-05-18 12:41:52 +0530

    test3

--- modules/create_app_service/main.tf
+++ modules/create_app_service/main.tf
@@ -18,6 +18,7 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id     = azurerm_service_plan.app_plan.id
 
   site_config {
+    always_on = false
     application_stack {
       docker_image_name   = var.docker_image
       docker_registry_url = var.docker_registry_url
